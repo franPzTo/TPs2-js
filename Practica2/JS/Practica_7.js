5a8fa29c5490d0c7de4efcff20d892194cd6f974
franPzTo <franperezt1@gmail.com>
2025-08-12 15:49:29 -0300

    Punto 7, Resuelto

---
+++ Practica2/JS/Practica_7.js
@@ -0,0 +1,12 @@
+let n = prompt("Escribe un número entre 0 y 50");
+if (n < 0 || n > 50) {
+    document.write("Número fuera de rango. Debe ser entre 0 y 50.");
+} else {
+    for (let i = n; i >= 1; i--) {
+        let r = "";
+        for (let j = 1; j <= i; j++) {
+            r += i;
+        }
+        document.write("<p>" + r + "</p>");
+    }
+}
